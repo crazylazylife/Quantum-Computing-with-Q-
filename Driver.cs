a80d6ea34eef36e377923eef66b56c726f7f33e0
Himadri Sankar Chatterjee <himadri.tito13@gmail.com>
2020-04-06 02:36:14 +0530

    Added entanglement files

---
+++ Driver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.Entanglement
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            var ones = 0;
+            var same = 0;
+            // We initialize a Quantum Simulator same as before.
+            using (var qsim = new QuantumSimulator())
+            {
+                // We run a loop for 1000 times to test the principle
+                for (int i = 0; i < 1000; i++)
+                {
+                    // We run the Entanglement operation and store the returned results in the respective varialbles 
+                    var (qone_state, qtwo_state) = Entanglement.Run(qsim).Result;
+                    // Check to see how many times we got state value 1. This will confirm our Superposition that collapses to state 0 or 1 with 50% probability for each
+                    if (qone_state == Result.One)
+                    {
+                        ones++;
+                    }
+                    // Next we perform the comparison to check if the state values of the qubit match. They should, if they are entangled and result in 100% matches
+                    if (qone_state == qtwo_state)
+                    {
+                        same++;
+                    }
+                }
+            }
+            // Fancy printing the results.
+            Console.WriteLine("Entanglement Results: ");
+            Console.WriteLine($"\t   One: {ones}");
+            Console.WriteLine($"\t  Zero: {1000 - ones}");
+            Console.WriteLine($"\t Same: {same / 1000 * 100}%");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Entanglement.qs
@@ -0,0 +1,42 @@
+﻿namespace Quantum.Entanglement
+{
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Canon;
+    
+    // Define the Entanglement operation that returns the mesurement of the state of the two qubits
+    operation Entanglement () : (Result, Result) {      // The two return types are of Result
+
+        // We initialize two variables to hold the value of the resultant state of the qubits after measurement
+        mutable qone_state = Zero;
+        mutable qtwo_state = Zero;
+
+        // We initialize the two qubits within the using block. This helps in Garbage Collection
+        using ((qubitOne, qubitTwo) = (Qubit(), Qubit())) {
+            // Perform operation to convert the first qubit to a superposition state.
+            // Note: We do not need to bring both the qubits to a superposition state
+            H(qubitOne);
+            // We perform the operation to Entangle the qubits
+            CNOT(qubitOne, qubitTwo);
+            // The sequnce of H() and CNOT() helps in entangling two qubits
+
+            // Finally we measure the state of the qubits with the M() and store them in the variable
+            set qone_state = M(qubitOne);
+            set qtwo_state = M(qubitTwo);
+
+            // We also reset the two initialized qubit. Microsoft requires it.
+            Reset(qubitOne);
+            Reset(qubitTwo);
+		}
+        // Finally we return the results of the measurement.
+        return (qone_state, qtwo_state);
+    }
+}namespace Quantum.Entanglement {
+
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+
+    
+    operation HelloQ () : Unit {
+        Message("Hello quantum world!");
+    }
+}
