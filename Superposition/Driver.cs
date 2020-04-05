7e95a616e3a58f68481e8381b0c1bacce22f3a68
Himadri Sankar Chatterjee <himadri.tito13@gmail.com>
2020-04-06 02:37:51 +0530

    Add files via upload

---
+++ Superposition/Driver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.Superposition
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            int ones = 0;
+            // We initialize a Quantum Simulator same as before
+            using (var qsim = new QuantumSimulator())
+            {
+                // We run a loop for 1000 times to test the phenomenon
+                for (int i = 0; i < 1000; i++)
+                {
+                    // We run the Superposition operation and store the returned result in the respective varialble 
+                    var result = Superposition.Run(qsim).Result;
+                    // Check if the value of the resultant state is 1
+                    if (result == Result.One)
+                    {
+                        ones++;
+                    }
+                }
+            }
+
+            // Print the number of times the resultant state has assumed the value of 0 and 1. They should be nearly 50% of the time.
+            Console.WriteLine("Collapsed States: ");
+            Console.WriteLine($"One: {ones}");
+            Console.WriteLine($"Zero: {1000 - ones}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Superposition/Superposition.qs
@@ -0,0 +1,24 @@
+﻿namespace Quantum.Superposition
+{
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Canon;
+    
+    // Define the superposition function
+    operation Superposition () : Result {
+
+        // Initialize a variable to store the valeu of the resultant state
+        mutable state = Zero;
+        // Initialize a qubit
+        using (qubit = Qubit()){
+            // Performing Hadamard operation to change the qubit to a superposition state
+            H(qubit);
+            // We measure the state of the qubit using M() and store it in the variable
+            set state = M(qubit);
+            
+            // We reset the qubit as Microsoft requires us to do so
+            Reset(qubit);
+		}
+        // Finally we return the value
+        return state;
+    }
+}
